08d5698b4e47a6a4421982892daeb3d802e37033
syfantasy <115546901+syfantasy@users.noreply.github.com>
2025-02-19 18:58:38 +0800

    Update SerpIkechan8370Tool.js

--- utils/tools/SerpIkechan8370Tool.js
+++ utils/tools/SerpIkechan8370Tool.js
@@ -28,39 +28,18 @@ export class SerpIkechan8370Tool extends AbstractTool {
     if (!source || !['google', 'bing', 'baidu', 'duckduckgo'].includes(source)) {
       source = 'bing'
     }
-
-    try {
-      const url = `https://serp.ikechan8370.com/${source}?q=${encodeURIComponent(q)}&lang=zh-CN&limit=${num}`
-      console.log('Requesting URL:', url) // 调试用
-
-      const response = await fetch(url, {
-        headers: {
-          'X-From-Library': 'ikechan8370'
-        }
-      })
-
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`)
+    let serpRes = await fetch(`https://serp.ikechan8370.com/${source}?q=${encodeURIComponent(q)}&lang=zh-CN&limit=${num}`, {
+      headers: {
+        'X-From-Library': 'ikechan8370'
       }
+    })
+    serpRes = await serpRes.json()
 
-      const serpRes = await response.json()
-      console.log('API Response:', serpRes) // 调试用
-
-      let res = serpRes.data || serpRes.results
-      if (!res) {
-        console.error('No data found in response:', serpRes)
-        return 'Sorry, no search results found.'
-      }
-
-      res?.forEach(r => {
-        delete r?.rank
-      })
-
-      return `the search results are here in json format:\n${JSON.stringify(res)} \n(Notice that these information are only available for you, the user cannot see them, you next answer should consider about the information)`
-    } catch (error) {
-      console.error('Search error:', error)
-      return `An error occurred during search: ${error.message}`
-    }
+    let res = serpRes.data || serpRes.results
+    res?.forEach(r => {
+      delete r?.rank
+    })
+    return `the search results are here in json format:\n${JSON.stringify(res)} \n(Notice that these information are only available for you, the user cannot see them, you next answer should consider about the information)`
   }
 
   description = 'Useful when you want to search something from the Internet. If you don\'t know much about the user\'s question, prefer to search about it! If you want to know further details of a result, you can use website tool'
