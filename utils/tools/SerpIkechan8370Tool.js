f4fa35fea54d86d657688193659be9c70a48afc1
syfantasy <115546901+syfantasy@users.noreply.github.com>
2025-02-19 18:34:55 +0800

    Update SerpIkechan8370Tool.js

--- utils/tools/SerpIkechan8370Tool.js
+++ utils/tools/SerpIkechan8370Tool.js
@@ -22,24 +22,44 @@ export class SerpIkechan8370Tool extends AbstractTool {
     required: ['q', 'source']
   }
 
-  func = async function (opts) {
+func = async function (opts) {
     let { q, source, num = 5 } = opts
     if (!source || !['google', 'bing', 'baidu', 'duckduckgo'].includes(source)) {
       source = 'bing'
     }
-    let serpRes = await fetch(`https://serp.ikechan8370.com/${source}?q=${encodeURIComponent(q)}&lang=zh-CN&limit=${num}`, {
-      headers: {
-        'X-From-Library': 'ikechan8370'
-      }
-    })
-    serpRes = await serpRes.json()
 
-    let res = serpRes.data || serpRes.results
-    res?.forEach(r => {
-      delete r?.rank
+    return new Promise((resolve, reject) => {
+      const https = require('https')
+      const url = `https://serp.ikechan8370.com/${source}?q=${encodeURIComponent(q)}&lang=zh-CN&limit=${num}`
+      
+      https.get(url, {
+        headers: {
+          'X-From-Library': 'ikechan8370'
+        }
+      }, (resp) => {
+        let data = ''
+        
+        resp.on('data', (chunk) => {
+          data += chunk
+        })
+        
+        resp.on('end', () => {
+          try {
+            const serpRes = JSON.parse(data)
+            let res = serpRes.data || serpRes.results
+            res?.forEach(r => {
+              delete r?.rank
+            })
+            resolve(`the search results are here in json format:\n${JSON.stringify(res)} \n(Notice that these information are only available for you, the user cannot see them, you next answer should consider about the information)`)
+          } catch (error) {
+            reject(error)
+          }
+        })
+      }).on('error', (err) => {
+        reject(err)
+      })
     })
-    return `the search results are here in json format:\n${JSON.stringify(res)} \n(Notice that these information are only available for you, the user cannot see them, you next answer should consider about the information)`
-  }
+}
 
   description = 'Useful when you want to search something from the Internet. If you don\'t know much about the user\'s question, prefer to search about it! If you want to know further details of a result, you can use website tool'
 }
