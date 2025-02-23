b2695025d4aece67596cf3c38c5630c75cbfca3a
syfantasy <115546901+syfantasy@users.noreply.github.com>
2025-02-23 09:35:39 +0800

    Update APTool.js

--- utils/tools/APTool.js
+++ utils/tools/APTool.js
@@ -7,7 +7,7 @@ export class APTool extends AbstractTool {
     properties: {
       prompt: {
         type: 'string',
-        description: 'draw prompt of midjourney, prefer to be in English. should be many keysentences split by comma.'
+        description: 'draw prompt of flux, prefer to be in English. should be many keywords split by comma.'
       }
     },
     required: []
@@ -24,21 +24,21 @@ export class APTool extends AbstractTool {
     let ap
     try {
       // eslint-disable-next-line camelcase
-      let { MJ_Painting } = await import('../../../siliconflow-plugin/apps/MJ_Painting.js')
-      ap = new MJ_Painting(e)
+      let { SF_Painting } = await import('../../../siliconflow-plugin/apps/SF_Painting.js')
+      ap = new SF_Painting(e)
     } catch (err) {
       try {
         // ap的dev分支改名了
         // eslint-disable-next-line camelcase
-        let { MJ_Painting } = await import('../../../siliconflow-plugin/apps/MJ_Painting.js')
-        ap = new MJ_Painting(e)
+        let { SF_Painting } = await import('../../../siliconflow-plugin/apps/SF_Painting.js')
+        ap = new SF_Painting(e)
       } catch (err1) {
         return 'the user didn\'t install sf-plugin. suggest him to install'
       }
     }
     try {
-      e.msg = '#mjp' + prompt
-      await ap.mj_draw(e)
+      e.msg = '#sf绘图' + prompt
+      await ap.sf_draw(e)
       return 'draw success, picture has been sent.'
     } catch (err) {
       return 'draw failed due to unknown error'
